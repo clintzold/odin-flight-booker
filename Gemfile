96a38643fdec11c6d95cacd411b71f78b14bd4b9
Clint Zold <clintzold@users.noreply.github.com>
2026-02-09 03:13:41 -0700

    add letter_opener and configure development ENV to use it

--- Gemfile
+++ Gemfile
@@ -57,6 +57,9 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+
+  # Use browser to view sent emails
+  gem "letter_opener"
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Deliver emails to letter_opener gem instead of actual email
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
